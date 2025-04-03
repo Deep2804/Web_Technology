4c1ba51b14cb12f85d5575e5a9c024571cf77162
deep2804 <deepak28042002singh@gmail.com>
2025-04-03 16:24:06 +0530

    Variables

---
+++ Javascript/1. Variables/fun.js
@@ -0,0 +1,42 @@
+//* console.log("I am External 1");
+
+//* document.write("Welcome to Javascript");
+
+//~ var a
+//~ a = 10
+//~ console.log(a);
+
+//^ var a
+//^ a = 20
+//^ console.log(a);
+
+
+//& declare
+//& let a 
+//& a = 20
+//& console.log(a)
+
+//! redeclare
+//!  let a 
+
+//* reinitialize
+//* a = 30
+//* console.log(a);
+
+//! declare and initialize at same time
+const a = 30
+console.log(a)
+
+//! cannot change the value if declared and initialized
+a = 4000
+
+
+
+
+
+
+
+
+
+
+
