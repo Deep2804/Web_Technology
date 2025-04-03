cdd3e45d798f76cce83fe999abedacaf65974ad0
deep2804 <deepak28042002singh@gmail.com>
2025-04-03 16:25:09 +0530

    Global Execution Context

---
+++ Javascript/3. GEC/index.js
@@ -0,0 +1,63 @@
+// console.log("Hello");
+// console.log("Hi");
+// console.log(a);
+//  console.log(b);
+
+//  let b = 100;
+//  console.log(b);
+//  var a = 10;
+//  console.log(a);
+
+//~  Conditional or loop
+//^  var - global
+//^ let - local
+//^  const - local
+
+//~  function
+//&  var, let, const - local
+
+//~  GLOBAL variable
+// var a1 = 10;
+// let b1 = 20;
+// const c1 = 30;
+
+//  console.log(a1,b1,c1);
+
+// if(10 > 2){
+//     var x = 100;
+//     let y = 200;
+//     const z = 300;
+//     console.log(a1,b1,c1);
+// }
+//  console.log(y);
+
+// for(let i=0;i<5;i++){
+//     console.log(a1,b1,c1);
+// }
+
+// function abc(){
+//     var m = 1000; //local
+//     let n = 2000; //local
+//     const o = 3000; //local
+//     console.log(m,n,o);
+// }
+// abc();
+// console.log(m);
+
+debugger
+console.log("start");
+console.log(a,b)
+console.log("Hello")
+var c = 300
+var a = 100
+console.log(c)
+c = 600
+var b;
+console.log(b)
+b = 400;
+console.log(a,c,b);
+var d = 500;
+console.log(d);
+a = 800
+console.log("End");
+
