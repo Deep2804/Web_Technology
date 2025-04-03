2ffec49a3587ee2265e022d2d41e64c7333fc45d
deep2804 <deepak28042002singh@gmail.com>
2025-04-03 16:24:36 +0530

    Data Types

---
+++ Javascript/2. DataTypes/script.js
@@ -0,0 +1,74 @@
+//! ========= Data Types in JS ==============
+//~ 1. Primitive Data types
+//~ 2. Non-primitive data type
+
+//& Primitive Type:
+//^ 1. Number
+//^ 2. String
+//^ 3. Boolean
+//^ 4. Null
+//^ 5. Undefined
+//^ 6. Big Int
+//^ 7. Symbol
+
+//& Non-primitive Types:
+//^ 1. Array
+//^ 2. Object
+//^ 3. Function
+
+
+//&  ================== Primitive Type: =========================
+//~ ========== Number ==================
+//*  var a = 10;
+//*  console.log(a);
+//*  console.log(typeof(a));
+
+//~ ========== BigInt ==================
+//! var b = 999999999999999999999n
+//! console.log(b);
+//! console.log(typeof b)
+
+//~ ========== String ==================
+//* var c = 'hello'
+//* console.log(c);
+//* console.log(typeof c);
+//* console.log(typeof typeof c);
+
+//~ ========== Boolean ==================
+//^ var d = true
+//^ console.log(d);
+//^ console.log(typeof d);
+
+//~ ========== Undefined ==================
+//& var e = undefined
+//& console.log(e);
+//& console.log(typeof e);
+
+//~ ========== Null ==================
+//? var f = null
+//? console.log(f);
+//? console.log(typeof f);
+
+//~ ========== Symbol ==================
+// var s1 = Symbol("Hello")
+// var s2 = Symbol("Hello")
+// console.log(s1);
+// console.log(s2);
+// console.log(s1 == s2)
+// console.log(s1 === s2);
+
+
+
+// var a = 10
+// var b = "10"
+// console.log(a == b);
+// console.log(a === b);
+
+// console.log(c);
+// var c =200;
+// console.log(c);
+
+//! Hoisting- JS move variable declaration part at the top of the code internally
+
+//! TDZ- temporal deadly zone
+
